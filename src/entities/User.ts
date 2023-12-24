923e6ee487b74bcd62a0d3f55c665daed991f93c
paikpaik <woduf9311@gmail.com>
2023-12-24 13:50:06 +0900

    Add: User Entity

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,1 +1,33 @@
+import { IsEmail, Length } from "class-validator";
+import { BeforeInsert, Column, Entity, Index, OneToMany } from "typeorm";
+import bcrypt from "bcryptjs";
 
+@Entity("users")
+export class User {
+  @Index()
+  @IsEmail(undefined, { message: "이메일 주소가 잘못되었습니다." })
+  @Length(1, 255, { message: "이메일 주소는 비워둘 수 없습니다." })
+  @Column({ unique: true })
+  email: string;
+
+  @Index()
+  @Length(3, 32, { message: "사용자 이름은 3자 이상이어야 합니다." })
+  @Column()
+  username: string;
+
+  @Index()
+  @Length(6, 255, { message: "비밀번호는 6자 이상이어야 합니다." })
+  @Column()
+  password: string;
+
+  @OneToMany(() => Post, (post) => post.user)
+  post: Post[];
+
+  @OneToMany(() => Vote, (vote) => votevotesr)
+  votes: Vote[];
+
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 6);
+  }
+}
