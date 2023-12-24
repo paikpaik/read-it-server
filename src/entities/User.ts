f329dfc7dd7f4d1c65b5f9fdc146c6e4d58eb56b
paikpaik <woduf9311@gmail.com>
2023-12-24 14:04:06 +0900

    Fix : User Entity extends fix

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,9 +1,10 @@
 import { IsEmail, Length } from "class-validator";
 import { BeforeInsert, Column, Entity, Index, OneToMany } from "typeorm";
 import bcrypt from "bcryptjs";
+import BaseEntity from "./Entity";
 
 @Entity("users")
-export class User {
+export class User extends BaseEntity {
   @Index()
   @IsEmail(undefined, { message: "이메일 주소가 잘못되었습니다." })
   @Length(1, 255, { message: "이메일 주소는 비워둘 수 없습니다." })
