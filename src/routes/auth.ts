f1d1682f581af43efa86ddea6c19eaf774f1c29d
paikpaik <woduf9311@gmail.com>
2023-12-25 01:21:46 +0900

    Add : register api add

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,45 @@
 import { Request, Response, Router } from "express";
+import User from "../entities/User";
+import { validate } from "class-validator";
+
+const mapError = (errors: Object[]) => {
+  return errors.reduce((prev: any, err: any) => {
+    prev[err.property] = Object.entries(err.constraints[0][1]);
+    return prev;
+  }, {});
+};
 
 const register = async (req: Request, res: Response) => {
   const { email, username, password } = req.body;
-  console.log("email", email);
+  try {
+    let errors: any = {};
+
+    const emailUser = await User.findOneBy({ email });
+    const usernameUser = await User.findOneBy({ username });
+
+    if (emailUser) errors.email = "이미 해당 이메일 주소가 사용되었습니다.";
+    if (usernameUser)
+      errors.username = "이미 해당 사용자 이름이 사용되었습니다.";
+
+    if (Object.keys(errors).length > 0) {
+      return res.status(400).json(errors);
+    }
+
+    const user = new User();
+    user.email = email;
+    user.username = username;
+    user.password = password;
+
+    errors = await validate(user);
+
+    if (errors.length > 0) return res.status(400).json(mapError(errors));
+
+    await user.save();
+    return res.json(user);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error });
+  }
 };
 
 const router = Router();
