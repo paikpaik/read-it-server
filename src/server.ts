e4a51ae8d91932fd93b0ade55925c21e75715dfc
paikpaik <woduf9311@gmail.com>
2023-12-25 00:45:29 +0900

    Add : cors 설정

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,24 @@
 import express from "express";
+import dotenv from "dotenv";
 import morgan from "morgan";
 import { AppDataSource } from "./data-source";
+import authRoutes from "./routes/auth";
+import cors from "cors";
 
+dotenv.config();
 const app = express();
+const origin = process.env.ORIGIN_URL;
 
+app.use(
+  cors({
+    origin,
+  })
+);
 app.use(express.json());
 app.use(morgan("dev"));
 
 app.get("/", (_, res) => res.send("running"));
+app.use("/api/auth", authRoutes);
 
 let port = 4000;
 app.listen(port, async () => {
